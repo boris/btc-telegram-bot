b929eb22b639a15f442464bda3b1528eff054074
Boris Quiroz <borisquiroz@pm.me>
2024-12-11 22:47:55 -0300

    Add percent change from CoinMarketCap

--- main.py
+++ main.py
@@ -7,10 +7,12 @@ import os
 import time
 
 
-url = "https://btc-api.borisquiroz.dev/btc"
+price_url = "https://btc-api.borisquiroz.dev/btc"
+data_url = "https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest"
 
 # get token from env variable
 BOT_TOKEN = os.getenv("TELEGRAM_TOKEN")
+CMC_TOKEN = os.getenv("CMC_TOKEN")
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     chat_id = update.message.chat_id
@@ -22,15 +24,25 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
 async def price(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     chat_id = update.message.chat_id
 
-    response = requests.get(url)
+    response = requests.get(price_url)
     r = response.json()
     value = r["price"]
 
+    data_request = requests.get(data_url, headers={"X-CMC_PRO_API_KEY": CMC_TOKEN})
+    data = data_request.json()
+    percent_change_24h = data["data"][0]["quote"]["USD"]["percent_change_24h"]
+    percent_change_7d = data["data"][0]["quote"]["USD"]["percent_change_7d"]
+    percent_change_30d = data["data"][0]["quote"]["USD"]["percent_change_30d"]
+    percent_change_60d = data["data"][0]["quote"]["USD"]["percent_change_60d"]
+    percent_change_90d = data["data"][0]["quote"]["USD"]["percent_change_90d"]
+
     gmtime_struct = time.gmtime()
     now = datetime(*gmtime_struct[:6])
+
+
     print(f"Chat ID: {chat_id} | BTC price: ${value} USD | Updated on {now} UTC.")
 
-    await update.message.reply_text(f"Current BTC price is ${value} USD.\nUpdated on {now} UTC.")
+    await update.message.reply_text(f"Current BTC price is ${value} USD.\nUpdated on {now} UTC.\n\nPercent change:\n24h: {percent_change_24h:.2f}%\n7d: {percent_change_7d:.2f}%\n30d: {percent_change_30d:.2f}%\n60d: {percent_change_60d:.2f}%\n90d: {percent_change_90d:.2f}%")
 
 
 def main():
